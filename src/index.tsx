129edb5455df203649fb723df05682013885c909
suboor khan <suboorkhan@suboors-MacBook-Air.local>
2022-12-25 05:18:58 +0530

    added css module

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,29 @@
 import * as React from 'react'
-import './styles.css';
+import styles from './styles.module.css'
 
 interface Props {
-  postsPerPage: number
-  totalPosts: number
+  postsPerPage: number;
+  totalPosts: number;
   paginate: (page: number) => void
 }
 
 export const SxPaginate = ({ postsPerPage, totalPosts, paginate }: Props) => {
-  const pageNumbers = []
+  const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i)
+    pageNumbers.push(i);
   }
 
   return (
     <nav>
-      <ul className={'pagination'}>
-        {pageNumbers.map((number) => (
+    <ul className={styles.pagination}>
+      {pageNumbers.map(number => (
           <a onClick={() => paginate(number)} className='page-link'>
             {number}
           </a>
-        ))}
-      </ul>
-    </nav>
+      
+      ))}
+    </ul>
+  </nav>
   )
 }
